a9f2e9785df52778cef39a4c0d2409d7389c7537
Daniel burns <danielburns@billericak12.com>
2022-03-09 12:05:58 -0500

    Daniel Burns

---
+++ src/com/company/Poem.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Poem {
+    private int lines;
+
+    public Poem(int n){
+        lines=n;
+    }
+
+    public int getNumlines(){
+        return lines;
+    }
+
+    public void printRhythm(){
+        System.out.println("Free Verse");
+    }
+}
